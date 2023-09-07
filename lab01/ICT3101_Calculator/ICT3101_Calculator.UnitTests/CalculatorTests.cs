5130982e4ea68174a94455c3d1e1ab5e774877b2
nobodomon <nobodomon@gmail.com>
2023-09-08 00:33:22 +0800

    lab01

---
+++ lab01/ICT3101_Calculator/ICT3101_Calculator.UnitTests/CalculatorTests.cs
@@ -0,0 +1,205 @@
+namespace ICT3101_Caculator.UnitTests
+{
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            _calculator = new Calculator();
+        }
+        [Test]
+        public void Add_WhenAddingTwoNumbers_ResultEqualToSum()
+        {
+            // Act
+            double result = _calculator.Add(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(30));
+        }
+        [Test]
+        public void Subtract_WhenSubtractingTwoNumbers_ResultEqualToDifference()
+        {
+            // Act
+            double result = _calculator.Subtract(20, 10);
+            // Assert
+            Assert.That(result, Is.EqualTo(10));
+        }
+        [Test]
+        public void Multiply_WhenMultiplyingTwoNumbers_ResultEqualToProduct()
+        {
+            // Act
+            double result = _calculator.Multiply(10, 20);
+            // Assert
+            Assert.That(result, Is.EqualTo(200));
+        }
+        [Test]
+        public void Divide_WhenDividingTwoNumbers_ResultEqualToQuotient()
+        {
+            // Act
+            double result = _calculator.Divide(20, 10);
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        //give me edge case test case for division
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, 0)]
+        public void Divide_WhenDividingByZero_ResultEqualToNaN(double a, double b)
+        {
+            // Assert
+            Assert.That(() => _calculator.Divide(20, 0), Throws.ArgumentException);
+        }
+
+
+        //test case for a factorial operation
+        [Test]
+        [TestCase(0, 1)]
+        [TestCase(1, 1)]
+        [TestCase(5, 120)]
+        public void Factorial_WhenFactorial_ResultEqualToFactorial(double a, double b)
+        {
+            // Act
+            double result = _calculator.Factorial(a);
+            // Assert
+            Assert.That(result, Is.EqualTo(b));
+        }
+
+        //test case for a factorial operation with negative numbers
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-5)]
+        public void Factorial_WhenFactorialNegativeNumber_ResultEqualToNaN(double a)
+        {
+            // Assert
+            Assert.That(() => _calculator.Factorial(a), Throws.ArgumentException);
+        }
+
+        //test case for calculating area of triangle
+        [Test]
+        [TestCase(1, 1, 0.5)]
+        [TestCase(2, 2, 2)]
+        [TestCase(3, 3, 4.5)]
+        public void AreaOfTriangle_WhenAreaOfTriangle_ResultEqualToArea(double a, double b, double c)
+        {
+            // Act
+            double result = _calculator.AreaOfTriangle(a, b);
+            // Assert
+            Assert.That(result, Is.EqualTo(c));
+        }
+
+        //test case for calculating area of triangle with negative numbers
+        [Test]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-1, -1)]
+        public void AreaOfTriangle_WhenAreaOfTriangleNegativeNumber_ResultEqualToNaN(double a, double b)
+        {
+            // Assert
+            Assert.That(() => _calculator.AreaOfTriangle(a, b), Throws.ArgumentException);
+        }
+
+        //test case for calculating area of circle
+        [Test]
+        [TestCase(1, 3.1415926535897932384626433832795)]
+        [TestCase(2, 12.566370614359172953850573533118)]
+        [TestCase(3, 28.274333882308138424341972731705)]
+        public void AreaOfCircle_WhenAreaOfCircle_ResultEqualToArea(double a, double b)
+        {
+            // Act
+            double result = _calculator.AreaOfCircle(a);
+            // Assert
+            Assert.That(result, Is.EqualTo(b));
+        }
+
+        //test case for calculating area of circle with negative numbers
+        [Test]
+        [TestCase(-1)]
+        [TestCase(-2)]
+        public void AreaOfCircle_WhenAreaOfCircleNegativeNumber_ResultEqualToNaN(double a)
+        {
+            // Assert
+            Assert.That(() => _calculator.AreaOfCircle(a), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void UnknownFunctionA_WhenGivenTest0_Result()
+        {
+            // Act
+            double result = _calculator.UnknownFunctionA(5, 5);
+            // Assert
+            Assert.That(result, Is.EqualTo(120));
+        }
+        [Test]
+        public void UnknownFunctionA_WhenGivenTest1_Result()
+        {
+            // Act
+            double result = _calculator.UnknownFunctionA(5, 4);
+            // Assert
+            Assert.That(result, Is.EqualTo(120));
+        }
+        [Test]
+        public void UnknownFunctionA_WhenGivenTest2_Result()
+        {
+            // Act
+            double result = _calculator.UnknownFunctionA(5, 3);
+            // Assert
+            Assert.That(result, Is.EqualTo(60));
+        }
+        [Test]
+        public void UnknownFunctionA_WhenGivenTest3_ResultThrowArgumnetException()
+        {
+            // Act
+            // Assert
+            Assert.That(() => _calculator.UnknownFunctionA(-4, 5), Throws.ArgumentException);
+        }
+        [Test]
+        public void UnknownFunctionA_WhenGivenTest4_ResultThrowArgumnetException()
+        {
+            // Act
+            // Assert
+            Assert.That(() => _calculator.UnknownFunctionA(4, 5), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void UnknownFunctionB_WhenGivenTest0_Result()
+        {
+        // Act
+        double result = _calculator.UnknownFunctionB(5, 5);
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void UnknownFunctionB_WhenGivenTest1_Result()
+        {
+        // Act
+        double result = _calculator.UnknownFunctionB(5, 4);
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+        }
+        [Test]
+        public void UnknownFunctionB_WhenGivenTest2_Result()
+        {
+        // Act
+        double result = _calculator.UnknownFunctionB(5, 3);
+        // Assert
+        Assert.That(result, Is.EqualTo(10));
+        }
+        [Test]
+        public void UnknownFunctionB_WhenGivenTest3_ResultThrowArgumnetException()
+        {
+        // Act
+        // Assert
+        Assert.That(() => _calculator.UnknownFunctionB(-4,5), Throws.ArgumentException);
+        }
+        [Test]
+        public void UnknownFunctionB_WhenGivenTest4_ResultThrowArgumnetException()
+        {
+        // Act
+        // Assert
+        Assert.That(() => _calculator.UnknownFunctionB(4,5), Throws.ArgumentException);
+        }
+    }
+}
---
+++ lab01/ICT3101_Calculator/ICT3101_Calculator/Calculator.cs
@@ -0,0 +1,112 @@
+public class Calculator
+{
+    public Calculator() { }
+    public double DoOperation(double num1, double num2, string op)
+    {
+        double result = double.NaN; // Default value
+                                    // Use a switch statement to do the math.
+        switch (op)
+        {
+            case "a":
+                result = Add(num1, num2);
+                break;
+            case "s":
+                result = Subtract(num1, num2);
+                break;
+            case "m":
+                result = Multiply(num1, num2);
+                break;
+            case "d":
+                // Ask the user to enter a non-zero divisor.
+                result = Divide(num1, num2);
+                break;
+            case "f":
+                result = Factorial(num1);
+                break;
+            // Return text for an incorrect option entry.
+            default:
+                break;
+        }
+        return result;
+    }
+    public double Add(double num1, double num2)
+    {
+        return (num1 + num2);
+    }
+    public double Subtract(double num1, double num2)
+    {
+        return (num1 - num2);
+    }
+    public double Multiply(double num1, double num2)
+    {
+        return (num1 * num2);
+    }
+    public double Divide(double num1, double num2)
+    {   
+        if(num1  == 0 || num2 == 0)
+        {
+            throw new ArgumentException("Cannot divide by zero");
+        }
+
+        return (num1 / num2);
+    }
+
+    public double Factorial(double num1)
+    {   
+        //if num1 is negative, throw exception
+        if(num1 < 0)
+        {
+            throw new ArgumentException("Cannot factorial negative number");
+        }
+
+        //if num1 is 0, return 1
+        if(num1 == 0)
+        {
+            return 1;
+        }
+        
+        return num1 * Factorial(num1 - 1);
+    }
+
+    public double AreaOfTriangle(double num1, double num2)
+    {
+        if(num1 <= 0 || num2 <= 0)
+        {
+            throw new ArgumentException("Cannot have negative or zero side length");
+        }
+
+        return (num1 * num2) / 2;
+    }
+
+    public double AreaOfCircle(double num1)
+    {
+        if(num1 <= 0)
+        {
+            throw new ArgumentException("Cannot have negative or zero radius");
+        }
+
+        return Math.PI * (num1 * num1);
+    }
+
+    public double UnknownFunctionA(double num1, double num2)
+    {
+        if(num1 < num2){
+            throw new ArgumentException("Cannot have num1 greater than num2");
+        }
+
+        //Permutation
+        
+        return Divide(Factorial(num1), Factorial(Subtract(num1, num2)));
+    }
+
+    public double UnknownFunctionB(double num1, double num2)
+    {
+        if(num1 < num2){
+            throw new ArgumentException("Cannot have num1 greater than num2");
+        }
+
+        //Binomial Coefficient
+
+        return Divide(Factorial(num1), Multiply(Factorial(num2), Factorial(Subtract(num1, num2))));
+    }
+}
